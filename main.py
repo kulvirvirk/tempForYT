fdf8593d1692bcfff317cc9222faf1a92968707e
KVirkDev <kvirkdev@gmail.com>
2020-06-14 01:36:17 +0000

    YT eg tem Code - input function

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+print("I am printing this text using a built-in print function!")
 
-print("I am printing this text using the built-in print function.")
+value = input("Please type something: ")
+print("You just typed: " + value)
+
+user_name1 = "Doolu"
+print("User1's name is: " + user_name1)
+
+user_name = input("Please enter your name: ")
+user_age = input("Hello " + user_name + ", Please enter your age: ")
+print("User name is: " + user_name + 
+      "\nUser’s age is: " + user_age)
